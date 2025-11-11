b62e751cbf28171b1d5028679fed95a721c019ac
olavma <olavma2004@gmail.com>
2025-11-11 15:38:41 +0100

    New dev cleaned

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio de OlavMa
-Este es el repositorio de mi portfolio que se encuentra en <a href="https://olavma.github.io">olavma.github.io</a>
--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  outDir: 'docs'
-});
